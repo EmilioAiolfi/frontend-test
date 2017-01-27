e1773f11cad17e25a324a1ec6ef11fc89e72f8a0
Emilio Aiolfi <emilio.neto@walmart.com>
2017-01-27 03:41:35 -0200

    Add folder containers and some containers

---
+++ client/containers/ChatContainer/index.jsx
@@ -0,0 +1,14 @@
+// Libs
+import React from 'react';
+
+const ChatContainer = React.createClass({
+  displayName: 'ChatContainer',
+
+  render() {
+    return (
+      <h1>{' Chat Container'} </h1>
+    );
+  }
+});
+
+export default ChatContainer;
---
+++ client/containers/HomeContainer/index.jsx
@@ -0,0 +1,14 @@
+// Libs
+import React from 'react';
+
+const HomeContainer = React.createClass({
+  displayName: 'HomeContainer',
+
+  render() {
+    return (
+      <h1>{' Home Container'} </h1>
+    );
+  }
+});
+
+export default HomeContainer;
