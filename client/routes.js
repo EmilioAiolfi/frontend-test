1df108b36a080960ba72c587abe5fc551b28096b
Emilio Aiolfi <emilio.neto@walmart.com>
2017-01-27 17:32:25 -0200

    Adjust for pages not found, render show 'not found' and not react-router

--- client/routes.js
+++ client/routes.js
@@ -9,13 +9,10 @@ import CathoTemplate from './templates/Catho';
 import HomeContainer from './containers/HomeContainer';
 import ChatContainer from './containers/ChatContainer';
 
-import NotFoundPage from './components/NotFoundPage';
-
 const routes = (
   <Route path="/site/home/chat" component={ CathoTemplate }>
     <IndexRoute component={ HomeContainer } />
     <Route path='section1' component={ChatContainer} />
-    <Route path="*" component={ NotFoundPage } />
   </Route>
 );
 
