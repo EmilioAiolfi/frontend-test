b082d8e57af747f9bcce709b6ca91b5a1df76b94
Emilio Aiolfi <emilio.neto@walmart.com>
2017-01-27 03:11:30 -0200

    Add more info in config

--- config/index.js
+++ config/index.js
@@ -1,10 +1,26 @@
-var path = require("path");
+import path from "path";
 
-module.exports = {
+const CSS_CHUNK_NAMING = '[name]__[local]___[hash:base64:5]';
+const STYLE_LOADERS = [
+  'css?modules&importLoaders=1&localIdentName=' + CSS_CHUNK_NAMING,
+  'postcss-loader',
+  'sass?&sourceMap',
+];
+
+const config = {
     PORT: 3000,
+    ENV : process.env.NODE_ENV || 'development',
     VIEW: {
         VIEW_PATH: path.join(__dirname, "/../views/"),
         ENCODING: "utf8",
-        EXTENSION: "html"
+        EXTENSION: "dust"
+    },
+    WEBPACK: {
+        STYLE_LOADERS: STYLE_LOADERS,
+        PATHS: {
+            ENTRY_APP:  path.resolve(__dirname, '../client', 'app-client.js')
+        }
     }
 };
+
+export default (config);
