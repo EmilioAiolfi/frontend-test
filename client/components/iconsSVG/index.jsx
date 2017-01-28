f2a7c96465379da3f6f90dd3cd7fb8ee7f8d1a79
Emilio Aiolfi <emilio.neto@walmart.com>
2017-01-27 23:38:17 -0200

    Add component iconsSVG - Sprite SVG

---
+++ client/components/iconsSVG/index.jsx
@@ -0,0 +1,55 @@
+// ==========================================================================
+// IconsSVG
+// ==========================================================================
+
+//== Libs
+// ==========================================================================
+import svgxhr from 'webpack-svgstore-plugin/src/helpers/svgxhr';
+import React, { PropTypes } from 'react';
+import classnames from 'classnames/bind';
+
+const __svg__ = {
+  path: '../../images/svg/**/*.svg',
+  name: 'static/[hash].icon.svg'
+};
+
+svgxhr(__svg__);
+
+//== Styles
+// ==========================================================================
+import styles from './style.scss';
+const cx = classnames.bind(styles);
+
+//== Component
+// ==========================================================================
+const Icon = React.createClass({
+  displayName: 'Icon',
+
+  propTypes: {
+    glyph : PropTypes.string.isRequired,
+    height : PropTypes.number,
+    width : PropTypes.number,
+  },
+
+  getDefaultProps() {
+    return {
+      width: 20,
+      height: 20
+    };
+  },
+
+  render() {
+    const className = 'icon';
+    const { glyph, width, height } = this.props;
+
+    return (
+      <svg className={ cx(className) } width={ width } height={ height } role='img'>
+        <use xlinkHref={`#${glyph}`} />
+      </svg>
+    );
+  }
+
+});
+
+
+export default Icon;
