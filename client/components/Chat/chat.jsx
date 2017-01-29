09f7db7f858cc301370d2b286e9182057625ef3f
Emilio Aiolfi <emilio.neto@walmart.com>
2017-01-29 04:28:22 -0200

    Adjust Chat style - toolbar css

--- client/components/Chat/chat.jsx
+++ client/components/Chat/chat.jsx
@@ -22,16 +22,16 @@ const Chat = React.createClass({
   },
 
   render() {
-
+    const toolBarIconColor = '#80AED2';
     return (
       <div className={ cx('chat') }>
         
         <div className={ cx('chat-toolbar') }>
-          <span className="chat-toolbar-title">{ 'Vaga: desenvolvedor Front-end' }</span>
-          <div className="chat-toolbar-actions">
-            <Icon glyph="icon-close" width={ 15 } height={ 15 } />
-            <Icon glyph="icon-minimize" width={ 15 } height={ 15 } />
-            <Icon glyph="icon-restore" width={ 15 } height={ 15 } />
+          <span className={ cx('chat-toolbar-title') }>{ 'Vaga: desenvolvedor Front-end' }</span>
+          <div className={ cx('chat-toolbar-actions') }>
+            <Icon glyph="icon-close" className={ cx('chat-toolbar-icon') } width={ 15 } height={ 20 } fill={ toolBarIconColor } />
+            <Icon glyph="icon-minimize" className={ cx('chat-toolbar-icon') } width={ 15 } height={ 20 } fill={ toolBarIconColor } />
+            <Icon glyph="icon-restore" className={ cx('chat-toolbar-icon') } width={ 15 } height={ 20 } fill={ toolBarIconColor } />
           </div>
         </div>
         
