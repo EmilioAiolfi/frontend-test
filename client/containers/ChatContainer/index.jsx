da55df304ade2b33e1bf32a3d26af43ff6638f7d
Emilio Aiolfi <emilio.neto@walmart.com>
2017-01-30 12:02:26 -0200

    Adjust ChatContainer with fetchdata

--- client/containers/ChatContainer/index.jsx
+++ client/containers/ChatContainer/index.jsx
@@ -1,16 +1,57 @@
 // Libs
 import React from 'react';
+import CONFIG from '../../config';
 
 import Chat from '../../components/Chat';
 
 const ChatContainer = React.createClass({
   displayName: 'ChatContainer',
 
+  getInitialState() {
+    return {
+      data: [],
+      loading: true,
+      timer: null,
+    };
+  },
+
+  componentDidMount(){
+    const timer = setTimeout( () => {
+      this.dataFetch(CONFIG.API.ENDPOINT);
+    }, 2000 );
+
+    this.setTimer(timer);
+  },
+
+  componentWillUnmount(){
+    clearTimeout(this.state.timer);
+  },
+
+  dataFetch(url) {
+    fetch(url)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(response) {
+      this.setState({
+        data: response.talkMessages,
+        loading: false,
+      });
+    }.bind(this))
+    .catch(function(error) {
+      console.error(error);
+    });
+  },
+
+  setTimer(timer) {
+    this.setState({timer});
+  },
+
   render() {
     return (
       <div>
         <h1>{ 'Chat Container' } </h1>
-        <Chat />
+        <Chat data={ this.state.data } loading={ this.state.loading } />
       </div>
     );
   }
