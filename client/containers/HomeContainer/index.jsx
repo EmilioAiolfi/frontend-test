081b504415df420029eb28c1d2268d9d4ef3fe71
Emilio Aiolfi <emilio.neto@walmart.com>
2017-01-30 18:12:51 -0200

    Remove render of component Container

--- client/containers/HomeContainer/index.jsx
+++ client/containers/HomeContainer/index.jsx
@@ -1,14 +1,6 @@
 // Libs
 import React from 'react';
 
-const HomeContainer = React.createClass({
-  displayName: 'HomeContainer',
-
-  render() {
-    return (
-      <h1>{' Home Container'} </h1>
-    );
-  }
-});
+const HomeContainer = () => ( <h1>{' Home Container'} </h1> );
 
 export default HomeContainer;
