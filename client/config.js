645ea7116be59a7ee489655a618848a7b77e77fc
Emilio Aiolfi <emilio.neto@walmart.com>
2017-01-30 12:01:42 -0200

    Add file config for client

---
+++ client/config.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const config = {
+  STATIC_PATH: '/static/',
+  API: {
+    ENDPOINT: '/static/json/talk.json',
+    USER_IMAGE_PATH: '/static/images/users'
+  }
+};
+
+export default (config);
